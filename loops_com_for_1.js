c5573ba56ccb537b19233e257710e064289680fe
Eduardo Marinho <eduardo.marinho@kenzie.com.br>
2023-02-27 13:40:56 -0300

    done

---
+++ loops_com_for_1.js
@@ -0,0 +1,68 @@
+// Exercício 1
+
+function renderUpToTwenty () {
+    for (let i = 0; i <= 20; i++) {
+        console.log(i)
+    }
+}
+// renderUpToTwenty()
+
+// Exercício 2
+
+function tenInTenToAHundred() {
+    for (let i = 1; i <= 100; i++) {
+        if (i % 10 === 0) {
+            console.log(i)
+        }
+    }
+}
+// tenInTenToAHundred()
+
+// Exercício 3
+
+function oddUpToTwenty() {
+    for (let i = 0; i <= 20; i++) {
+        if (i % 2 !== 0) {
+            console.log(i)
+        }
+    }
+}
+// oddUpToTwenty()
+
+// Exercício 4
+
+function evenUpToTwenty() {
+    for (let i = 0; i <= 20; i++) {
+        if (i % 2 === 0) {
+            console.log(i)
+        }
+    }
+}
+// evenUpToTwenty()
+
+// Exercício 5
+
+function fromNegativeToPositive() {
+    for (let i = -10; i <= 0; i++) {
+        console.log(i)
+    }
+}
+// fromNegativeToPositive()
+
+// Exercício 6
+
+function inDescendingDirection() {
+    for (let i = 10; i >= 0; i--) {
+        console.log(i)
+    }
+}
+// inDescendingDirection()
+
+// Exercício 7
+
+function toSquare() {
+    for (let i = 1; i <= 10; i++) {
+        console.log(i * i)
+    }
+}
+// toSquare()
