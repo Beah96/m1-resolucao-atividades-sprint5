3d2ea407dec491d177805a0241e271df9a65a559
alessandravicentini <alessandravicentini01@gmailc.om>
2023-03-01 09:49:02 -0300

    feat: adicionando comentários na resolução

--- caixa_de_suprimentos.js
+++ caixa_de_suprimentos.js
@@ -7,30 +7,31 @@ let caixaDeSuprimentos = [higiene, alimentacao, farmacia, lazer]
 
 function suprimentos(){
 
-    if (caixaDeSuprimentos.length == 4){
+    if (caixaDeSuprimentos.length < 4){ // Verificando se a caixa tem menos de 4 seções
         
-        for (let i = 0; i < caixaDeSuprimentos.length; i++){
-
-            if (caixaDeSuprimentos[i].length < 5){
+        return 'Algum amigo ainda não retornou com os itens.'
+    
+    } else if (caixaDeSuprimentos.length > 4){ // Verificando se a caixa tem mais de 4 seções
+    
+        return 'Acho que temos um intruso.'
+    
+    } else { // Caso a caixa tenha 4 seções
+    
+        for (let i = 0; i < caixaDeSuprimentos.length; i++){ // Percorrendo as seções
+    
+            if (caixaDeSuprimentos[i].length < 5){ // Se a seção acessada possui menos que 5 itens
                 return `Precisamos de mais itens na seção ${[i]}.`
             }
-            if (caixaDeSuprimentos[i].length > 5){
+            if (caixaDeSuprimentos[i].length > 5){ // Se a seção acessada possui mais que 5 itens
                 return `Tem itens demais na seção ${[i]}, não precisamos de tantos.`
             }
-
+    
         }
-
-        return 'Podemos ir acampar'
-
-    } else if (caixaDeSuprimentos.length < 4){
-
-        return 'Algum amigo ainda não retornou com os itens.'
-
-    } else {
-
-        return 'Acho que temos um intruso.'
+    
+        return 'Podemos ir acampar' // Caso o loop termine e nenhum outro retorno aconteça, é seguro dizer que todas as caixas têm 5 itens
     }
 
 }
 
 console.log(suprimentos())
+
